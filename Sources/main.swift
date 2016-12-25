2122088690939455e6c9dda562af030b2e90c4ae
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2016-12-25 15:13:50 -0600

    Initial server implementation
     - Add Kitura as a dependency
     - Simple Hello World response

---
+++ Sources/main.swift
@@ -0,0 +1,12 @@
+import Kitura
+
+let router = Router()
+
+router.get("/") {
+  request, response, next in
+  response.send("Hello World!")
+  next()
+}
+
+Kitura.addHTTPServer(onPort: 8090, with: router)
+Kitura.run()
