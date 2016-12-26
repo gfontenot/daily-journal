4313b6eb16c30c7c2870dc5394a0b910e4321f0f
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2016-12-25 20:23:05 -0600

    Allow the specification of a date via query params

--- Sources/main.swift
+++ Sources/main.swift
@@ -9,12 +9,16 @@ let router = Router()
 router.add(templateEngine: StencilTemplateEngine())
 
 router.get("/") { request, response, next in
-  let context = question(for: .today)
+  let date = request.queryParameters["date"]
+    .flatMap(formatter.date(from:))
+
+  let context = question(for: date ?? .today)
     |> displayContext
 
   try! response.render("question.stencil", context: context).end()
   next()
 }
+
 
 router.get("/create") { request, response, next in
   let url = question(for: .today)
